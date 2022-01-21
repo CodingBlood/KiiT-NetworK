7b5432048ac8ad9f18cb0fb747cba835a08de60f
CodingBlood <kartik.agarwal.192.168@gmail.com>
2022-01-21 17:10:28 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div id="App">
+          <Navbar/>
+
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+          <h1>Laura Ipsum</h1>
+      </div>
   );
 }
 
---
+++ src/Components/Navbar.js
@@ -0,0 +1,18 @@
+import './stylesheets/navbar.css'
+
+function Navbar(){
+
+    return (
+        <div id="nav">
+            <div id="navbar"><div id="nav-tit">KiiT Network</div></div>
+            <div id="navbar"><div id="nav-btn"><a href=" /"><span id="nav-btn-l">Trolls</span></a></div></div>
+            <div id="navbar"><div id="nav-btn"><a href="/"><span id="nav-btn-l">News</span></a></div></div>
+            <div id="navbar"><div id="nav-btn"><a href="/"><span id="nav-btn-l">Resources</span></a></div></div>
+            <div id="navbar"><a href="/"><span id="nav-btn-r">&nbsp;</span></a></div>
+            <div id="navbar"><div id="nav-btn"><a href="/"><span id="nav-btn-l">Login</span></a></div></div>
+            <div id="navbar"><div id="nav-btn"><a href="/"><span id="nav-btn-l">Signup</span></a></div></div>
+        </div>
+    );
+}
+
+export default Navbar;
